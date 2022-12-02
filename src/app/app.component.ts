7d417731528acf5183607e07001cd31732204e67
Zviad Mikeladze <zviadipf@gmail.com>
2022-12-02 13:46:00 +0400

    Data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  serverElements = [];
-  
+  serverElements = [{ type: 'server', name: 'test', content: 'testing' }];
 }
